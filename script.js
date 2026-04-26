d3b844beeab841533c71836cccf8a552d1e6399b
H.Movies official <zidirajpoot786a@gmail.com>
2026-04-26 13:11:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
-const container = document.querySelector(".movies"); // apna div class check kar lena
-
-async function loadMovies() {
-  try {
-    const res = await fetch("/.netlify/functions/movies");
-    const data = await res.json();
-
-    container.innerHTML = "";
-
-    data.results.forEach(movie => {
-      const div = document.createElement("div");
-      div.classList.add("movie");
-
-      div.innerHTML = `
-        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
-        <h3>${movie.title}</h3>
-      `;
-
-      container.appendChild(div);
-    });
-
-  } catch (err) {
-    console.log("Error:", err);
-  }
-}
-
-loadMovies();
+document.querySelectorAll('.movie').forEach(movie => {
+  movie.addEventListener('click', () => {
+    alert('Movie clicked');
+  });
+});
