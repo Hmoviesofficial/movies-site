0f5b62ce5be6a794b0088045d2bf14b575dbfcee
H.Movies official <zidirajpoot786a@gmail.com>
2026-04-26 12:09:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-moviesContainer.innerHTML = data.results
-  .map(
-    (movie) => `
-  <div class="movie">
-    <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
-    <h3>${movie.title}</h3>
-  </div>
-`
-  )
-  .join("");
+const container = document.querySelector(".movies"); // apna div class check kar lena
+
+async function loadMovies() {
+  try {
+    const res = await fetch("/.netlify/functions/movies");
+    const data = await res.json();
+
+    container.innerHTML = "";
+
+    data.results.forEach(movie => {
+      const div = document.createElement("div");
+      div.classList.add("movie");
+
+      div.innerHTML = `
+        <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" />
+        <h3>${movie.title}</h3>
+      `;
+
+      container.appendChild(div);
+    });
+
+  } catch (err) {
+    console.log("Error:", err);
+  }
+}
+
+loadMovies();
