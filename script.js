4fc16a0c485cfc952afbb5891f19221868bf16a3
H.Movies official <zidirajpoot786a@gmail.com>
2026-04-26 11:42:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-fetch("/.netlify/functions/movies")
-.then(res => res.json())
-.then(data => {
-    showMovies(data.results);
-    setHero(data.results[0]);
-});
+async function getMovies() {
+  const res = await fetch("/.netlify/functions/movies");
+  const data = await res.json();
+
+  const moviesContainer = document.getElementById("movies");
+
+  moviesContainer.innerHTML = data.results
+    .map(
+      (movie) => `
+    <div>
+      <h3>${movie.title}</h3>
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" width="150"/>
+    </div>
+  `
+    )
+    .join("");
+}
+
+getMovies();
