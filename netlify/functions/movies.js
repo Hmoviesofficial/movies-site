bf6c4c7742160b6b7b48d52077820007a2acb8ef
H.Movies official <zidirajpoot786a@gmail.com>
2026-04-26 11:37:19 +0500

    Update movies.js

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -1,11 +1,19 @@
 exports.handler = async function () {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      results: [
-        { title: "Movie 1" },
-        { title: "Movie 2" }
-      ]
-    })
-  };
+  try {
+    const res = await fetch(
+      "https://api.themoviedb.org/3/trending/movie/week?api_key=f409489a42d61f514484fa959f76fec7"
+    );
+
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch movies" }),
+    };
+  }
 };
